8d6fa959f5a530887f1c76d5d6c786ffeb1837d8
Simon Sabin <Simon@sabin.io>
2021-07-13 09:39:41 +0100

    fix stuff

--- src/Pipeline.Sql.Deploy.Tests/Test-IsPreviousDeploySettingsFileMissing.Tests.ps1
+++ src/Pipeline.Sql.Deploy.Tests/Test-IsPreviousDeploySettingsFileMissing.Tests.ps1
@@ -1,25 +1,28 @@
 param($ModulePath, $SourcePath, $ProjectName)
 
 BeforeDiscovery {
-	if (-not $PSBoundParameters.ContainsKey("ProjectName")) { $ProjectName = (get-item $PSScriptRoot).basename -replace ".tests", "" }
-	if (-not $PSBoundParameters.ContainsKey("ModulePath")) { $ModulePath = "$PSScriptRoot\..\$ProjectName.module" }
-	if (-not  $PSBoundParameters.ContainsKey("SourcePath")) { $SourcePath = "$ModulePath" }
+
+
+}
+BeforeAll{
+	if (-not (Test-Path variable:ProjectName)) { $ProjectName = (get-item $PSScriptRoot).basename -replace ".tests", "" }
+	if (-not (Test-Path variable:ModulePath)) { $ModulePath = "$PSScriptRoot\..\$ProjectName.module" }
+	if (-not  (Test-Path variable:SourcePath)) { $SourcePath = "$ModulePath" }
 
 	$CommandName = [IO.Path]::GetFileName($PSCommandPath).Replace(".Tests.ps1", "")
 	
 	$ModulePath = resolve-path $ModulePath
 	$SourcePath = Resolve-path $SourcePath
 	
+	get-module pipeline.sql.deploy | remove-module
 	. "$ModulePath/Functions/$CommandName.ps1"
-}
-BeforeAll{
 	Set-StrictMode -Version 1.0
+	$env:psmodulepath =""
 }
 Describe "Checking Previous Settings"  {
 	
 	It "Should get settings from database"{
 		Test-IsPreviousDeploySettingsFileMissing 
-
     }
 
 }
