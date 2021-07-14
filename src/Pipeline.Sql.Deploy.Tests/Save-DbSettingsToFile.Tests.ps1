7f7d65f88a3fb3c3c9efbb9869e3240e277c63d6
Simon Sabin <1209963+simonsabin@users.noreply.github.com>
2021-07-14 09:52:21 +0100

    Fails if parent folder for settings file doesn't exist (#16)
    * Fails if parent folder for settings file doesn't exist
    Fixes #15

---
+++ src/Pipeline.Sql.Deploy.Tests/Save-DbSettingsToFile.Tests.ps1
@@ -0,0 +1,31 @@
+param(
+    $ModulePath,
+    $ProjectName
+)
+BeforeDiscovery{
+    Write-Verbose "Module path Beforedisco - $ModulePath"-verbose
+}
+BeforeAll {
+	Set-StrictMode -Version 1.0
+    $CommandName = [IO.Path]::GetFileName($PSCommandPath).Replace(".Tests.ps1", "")
+    if (-not (Test-path  Variable:\ProjectName)) { $ProjectName = (get-item $PSScriptRoot).basename -replace ".tests", "" }
+    if (-not (Test-path  Variable:\ModulePath) -or "$ModulePath" -eq "") {$ModulePath = "$PSScriptRoot\..\$ProjectName.module" }
+
+#    get-module $ProjectName | Remove-Module -Force
+    . $ModulePath\Functions\Internal\Get-SettingsAsJson.ps1
+    . $ModulePath\Functions\$CommandName.ps1
+}
+
+Describe 'Save-DbSettingsToFile' {
+    It 'should create parent folder if not exists' {
+        $Parent = [io.path]::Combine("TestDrive:","ParentFolder")
+        $File = [io.path]::Combine($Parent,"Settings.file")
+        if (test-path $Parent){Remove-item $Parent -Force -Recurse | out-null}
+
+        Save-DbSettingsToFile -DBDeploySettingsFile $File -settings @{Server="localhost";Database="TestDB";User="TestUser";Password="TestPassword"}
+
+        test-path $Parent | should -be $true
+        Test-Path $file | should -be $true
+
+    }
+}
