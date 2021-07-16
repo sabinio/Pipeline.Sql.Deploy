53f23ac766cafc4ba7da77c74ec32875cb5ebb10
Simon Sabin <Simon@sabin.io>
2021-07-16 21:06:59 +0100

    remove debug

--- .build/pipeline.install-tools.ps1
+++ .build/pipeline.install-tools.ps1
@@ -16,12 +16,12 @@ function Repair-PSModulePath {
     }
     Write-host "Repair PSMOdulePath - End"
 }
-$DebugPreference="continue"
-Register-PackageSource -Location https://www.powershellgallery.com/api/v2 -providerName NUget -name NugetPS -Force -Verbose:$VerbosePreference
+#$DebugPreference="continue"
+#Register-PackageSource -Location https://www.powershellgallery.com/api/v2 -providerName NUget -name NugetPS -Force -Verbose:$VerbosePreference
 
 #if ((Get-PSRepository -Name PSGallery -Verbose:$VerbosePreference).InstallationPolicy -ne "Trusted"){set-psrepository -name PSGallery -InstallationPolicy Trusted -Verbose:$VerbosePreference}
 
-#if (Get-PSRepository PowershellGalleryTest -Verbose:$VerbosePreference -ErrorAction SilentlyContinue){Unregister-PSRepository PowershellGalleryTest}
+if (Get-PSRepository PowershellGalleryTest -Verbose:$VerbosePreference -ErrorAction SilentlyContinue){Unregister-PSRepository PowershellGalleryTest}
 
 $LatestVersion = "0.2.165" #This is just too slow (Find-Module Pipeline.Tools -Repository "PSGallery").Version
 Write-Host "Getting Pipeline.Tools module $LatestVersion"
