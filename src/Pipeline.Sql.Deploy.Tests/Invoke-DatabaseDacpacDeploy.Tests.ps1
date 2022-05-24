758f0e84f8d4abd4ffd23144b14f7a5c66284b07
Simon Sabin <Simon@sabin.io>
2022-05-24 21:43:10 +0100

    Doesn't throw an error if dacpac deployment fails Fixes #37

--- src/Pipeline.Sql.Deploy.Tests/Invoke-DatabaseDacpacDeploy.Tests.ps1
+++ src/Pipeline.Sql.Deploy.Tests/Invoke-DatabaseDacpacDeploy.Tests.ps1
@@ -8,7 +8,7 @@ BeforeAll {
     
     Get-Module $ProjectName | Remove-Module  
 
-    if (-not (Test-path  Variable:\ModulePath) -or "$ModulePath" -eq "") {$ModulePath = "$PSScriptRoot\..\$ProjectName.module" }
+    if (-not (Test-path  Variable:\ModulePath) -or "$ModulePath" -eq "") { $ModulePath = "$PSScriptRoot\..\$ProjectName.module" }
   
     . $ModulePath\Functions\$CommandName.ps1
     . $ModulePath\Functions\Write-DbDeployParameterLog.ps1
@@ -16,9 +16,11 @@ BeforeAll {
     . $ModulePath\Functions\Internal\Get-DeployPropertiesHash.ps1
     . $ModulePath\Functions\Internal\Get-DefaultSettingsToCheck.ps1
     . $ModulePath\Functions\Internal\Convert-ToSQLPackageSafeString.ps1
-    . $ModulePath\Functions\Internal\Get-ReferencedDacpacsFromModel.ps1
     . $ModulePath\Functions\Internal\Get-ModelChecksum.ps1
+    . $ModulePath\Functions\Internal\Get-ReferencedDacpacsFromModel.ps1
+    
 }
+
 Describe 'Invoke-DatabaseDacpacDeploy' {
     BeforeAll {
         $PSModuleAutoloadingPreference = "none"
@@ -31,22 +33,45 @@ Describe 'Invoke-DatabaseDacpacDeploy' {
     Context 'Return values' {
 
         It "Returns list of scripts created" {
-                mock invoke-command {0}
-                $sqlpackagePath = "sqlpackage"
-                $folder = [System.io.path]::Combine("TestDrive:","ReturnValues","out")
-                if (test-path $folder){remove-item $folder -Force |out-null}
-                $dacpac = [System.io.path]::Combine("TestDrive:","dacpac","test.dacpac")
-                new-item  TestDrive:/dacpac -type directory -force | Out-Null
-                copy-item $PSScriptRoot/Test.dacpac $dacpac -Force 
+            mock invoke-command { 0 }
+            $sqlpackagePath = "sqlpackage"
+            $folder = [System.io.path]::Combine("TestDrive:", "ReturnValues", "out")
+            if (test-path $folder) { remove-item $folder -Force | out-null }
+            $dacpac = [System.io.path]::Combine("TestDrive:", "dacpac", "test.dacpac")
+            new-item  TestDrive:/dacpac -type directory -force | Out-Null
+            copy-item $PSScriptRoot/Test.dacpac $dacpac -Force 
                 
-                $targetDatabase = "ReturnValues"
-                $scripts = @("db.sql","master.sql")
-                $scripts | ForEach-Object{new-item -ItemType File ([System.io.path]::Combine($folder,$targetDatabase,$_)) -Force}
+            $targetDatabase = "ReturnValues"
+            $scripts = @("db.sql", "master.sql")
+            $scripts | ForEach-Object { new-item -ItemType File ([System.io.path]::Combine($folder, $targetDatabase, $_)) -Force }
 
-                $results = Invoke-DatabaseDacpacDeploy -dacpacfile $dacpac -sqlpackagePath $sqlpackagePath -action "script"  -scriptParentPath $folder -TargetServerName "." -TargetDatabaseName $targetDatabase -Variables @() -TargetTimeout 10 -CommandTimeout 100 -TargetIntegratedSecurity $true
+            $results = Invoke-DatabaseDacpacDeploy -dacpacfile $dacpac -sqlpackagePath $sqlpackagePath -action "script"  -scriptParentPath $folder -TargetServerName "." -TargetDatabaseName $targetDatabase -Variables @() -TargetTimeout 10 -CommandTimeout 100 -TargetIntegratedSecurity $true
                 
-                Assert-MockCalled invoke-command -Times 1 
-                $results.Scripts.name | should -Be $scripts
+            Assert-MockCalled invoke-command -Times 1 
+            $results.Scripts.name | should -Be $scripts
         }
+
+        It "When Dacpac is invalid" {
+            mock invoke-command {  $(&cmd /c dir bobby) } #simmulating a command failure
+            $sqlpackagePath = "sqlpackage"
+            $folder = [System.io.path]::Combine("TestDrive:", "ReturnValues", "out")
+            if (test-path $folder) { remove-item $folder -Force | out-null }
+            $dacpac = [System.io.path]::Combine("TestDrive:", "dacpac", "test.dacpac")
+            new-item  TestDrive:/dacpac -type directory -force | Out-Null
+            copy-item $PSScriptRoot/Test.dacpac $dacpac -Force 
+        
+            $targetDatabase = "ReturnValues"
+            $scripts = @("db.sql", "master.sql")
+            $scripts | ForEach-Object { new-item -ItemType File ([System.io.path]::Combine($folder, $targetDatabase, $_)) -Force }
+            $exception =$Null
+            try{
+                Invoke-DatabaseDacpacDeploy -dacpacfile $dacpac -sqlpackagePath $sqlpackagePath -action "script"  -scriptParentPath $folder -TargetServerName "." -TargetDatabaseName $targetDatabase -Variables @() -TargetTimeout 10 -CommandTimeout 100 -TargetIntegratedSecurity $true 
+            } 
+            catch {
+                $exception = $_
+            }
+            $Exception.Exception.message | Should -belike  "SqlPackage returned non-zero exit code*"
+        }
+
     }
 }
